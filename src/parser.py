ce40ea6afb719f1ca42a7f27ecd155835279de5d
HowToProgramBirds <dylananne.v.eck@gmail.com>
2021-12-22 15:53:54 +0100

    startup parser

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,5 @@
-from lexer import make_tokens, Lexer_token
+import lexer
+#from lexer import make_tokens, Lexer_token
 from typing import List, Tuple
 
 class Parser_token:
@@ -11,15 +12,38 @@ class Parser_token:
         return "<{}>: (line number:{})".format(type(self).__name__, self.nmbr_line)
 
         
-def parse_tokens(tokens: List[Lexer_token]) -> List[Parser_token]:
+def parse_tokens(tokens: List[lexer.Lexer_token]) -> List[Parser_token]:
 
     if len(tokens) == 0:
         return []
-
+    
+    token_type = type(tokens[0])
+    print(token_type)
+    if token_type == lexer.Func_token:
+        print("a")
+        parse_tokens(tokens[1:])
+    elif token_type == lexer.Var_token:
+        print("b")
+        parse_tokens(tokens[1:])
+    elif token_type == lexer.Beg_scope_token:
+        print("c")
+        parse_tokens(tokens[1:])
+    elif token_type == lexer.Print_token:
+        print("d")
+        parse_tokens(tokens[1:])
+    elif token_type == lexer.String_token:
+        print("e")
+        parse_tokens(tokens[1:])
+    elif token_type == lexer.Endline_token:
+        print("f")
+        parse_tokens(tokens[1:])
+    elif token_type == lexer.End_scope_token:
+        print("g")
+        parse_tokens(tokens[1:])
     return tokens
 
 with open("../examples/hello_world.txt", "r") as f:
-    hello_word_tokens = make_tokens(f.read())
+    hello_word_tokens = lexer.make_tokens(f.read())
     for token in hello_word_tokens:
         print(token)
     parsed_hello_world_tokens = parse_tokens(hello_word_tokens)
