a38e13b3c8454838a04976696321015f26889e21
HowToProgramBirds <dylananne.v.eck@gmail.com>
2022-01-11 15:44:30 +0100

    update parser

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
-from lexer import make_tokens, Lexer_token
-from typing import List, Tuple
+from lexer import *
+from typing import List, Tuple, Type
 
 class Parser_token:
     def __init__(self, nmbr_line):
@@ -10,12 +10,28 @@ class Parser_token:
     def __repr__(self):
         return "<{}>: (line number:{})".format(type(self).__name__, self.nmbr_line)
 
+def eat_token(tokens: List[Lexer_token], type_of_token: Type):
+    if len(tokens) == 0:
+        return None, tokens
+    elif isinstance(tokens[0], type_of_token):
+        return tokens[0], tokens[1:]
+    else:
+        return None, tokens
+
+        
+def parse_function(tokens: List[Lexer_token]):
+    function_var, tokens = eat_token(tokens[1:], lexer.Var_token)
+    parameters, tokens = parse_parameters(tokens)
+    
         
 def parse_tokens(tokens: List[Lexer_token]) -> List[Parser_token]:
 
     if len(tokens) == 0:
         return []
 
+    elif tokens[0] == Func_token:
+        parse_function
+        return parse_tokens(tokens_rest)
     return tokens
 
 with open("../examples/hello_world.txt", "r") as f:
